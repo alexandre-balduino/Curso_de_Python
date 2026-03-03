aa92b2e674ebbfe59d77464c6c7abb7470d4e67c
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-03 14:48:19 -0300

    Resolvi o ex_016 do mundo 4

---
+++ mundo_4_poo/ex_016/ex_016.py
@@ -0,0 +1,28 @@
+'''
+Desafio 016
+
+Crie a classe Funcionario, 
+onde podemos cadastrar nome, 
+setor e cargo. 
+Crie tambem um método que permita 
+ao funcionário se apresentar.
+'''
+
+from rich import print
+
+class Funcionario:
+    empresa = "Curso em Vídeo"
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+    
+    
+    def apresentar(self):
+        return f":wave: Olá, sou [blue] {self.nome}[/] e sou {self.cargo} do setor {self.setor} da empresa {__class__.empresa}"
+
+
+maria = Funcionario("Maria", "Administração", "Diretora")
+pedro = Funcionario("Pedro", "TI", "Programador")
+print(maria.apresentar())
+print(pedro.apresentar())
