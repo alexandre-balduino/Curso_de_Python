4f092689df4409c0e06539314bc36944b73d19e1
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-02 22:43:46 -0300

    Resolvi o exercício 006

---
+++ mundo_1/ex_006.py
@@ -0,0 +1,15 @@
+'''
+Desafio 006
+
+Crie um algoritmo que leia um número 
+e mostre o seu dobro, triplo e 
+a raiz quadrada.
+'''
+
+num = int(input("Digite um número: "))
+dob = num * 2
+tri = num * 3
+raiz = num ** 0.5
+print(f"O dobro de {num} é {dob}")
+print(f"O triplo de {num} é {tri}")
+print(f"E a raiz quadrada de {num} é {raiz}")
