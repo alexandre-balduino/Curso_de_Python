660bfa36fb246f6d3f734cf7b050995d468f4ef3
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-03 15:01:37 -0300

    Resolvi o ex_025 do mundo 1

--- mundo_1/ex_025.py
+++ mundo_1/ex_025.py
@@ -5,3 +5,6 @@ Crie um programa que leia o nome
 de uma pessoa e diga se ela tem “
 SILVA” no nome.
 '''
+
+nome = input("Digite seu nome: ")
+print(f"Seu nome tem SILVA? {'SILVA' in nome.upper()}")
