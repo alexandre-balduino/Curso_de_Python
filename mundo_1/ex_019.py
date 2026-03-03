aae0336b69409e300ab4275fe99ca09dac18acb6
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-02 22:50:21 -0300

    Resolvi o exercício 019

---
+++ mundo_1/ex_019.py
@@ -0,0 +1,20 @@
+'''
+Desafio 019
+
+Um professor quer sortear um dos 
+seus quatro alunos para apagar o 
+quadro. Faça um programa que ajude 
+ele, lendo o nome dos alunos e 
+escrevendo na tela o nome do 
+escolhido.
+'''
+
+from random import  choice
+
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+alunos = [aluno1, aluno2, aluno3, aluno4]
+escolhido = choice(alunos)
+print(f"O aluno escolhido foi {escolhido}")
