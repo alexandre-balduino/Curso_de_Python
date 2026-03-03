303aa27306243c3c2185a362f1bf1f20542341c3
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-02 22:49:25 -0300

    Resolvi o exercício 014

---
+++ mundo_1/ex_014.py
@@ -0,0 +1,12 @@
+'''
+Desafio 014
+
+Escreva um programa que converta 
+uma temperatura digitando em 
+graus Celsius e converta para 
+graus Fahrenheit.
+'''
+
+c = float(input("Digite a temperatura em graus Celcius: "))
+f = c * 1.8 + 32
+print(f"{c}° graus Celcius equivale a {f}° graus Fahreinheit")
