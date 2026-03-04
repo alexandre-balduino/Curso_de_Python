1d5285ad13fcd58980dbf79d835066b25374c7a9
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-04 17:45:08 -0300

    Resolvi o exercício 031

--- mundo_1/ex_031.py
+++ mundo_1/ex_031.py
@@ -8,3 +8,10 @@ cobrando R$0,50 por Km para viagens
 de até 200Km e R$0,45 para viagens 
 mais longas.
 '''
+
+km = int(input("Digite a distância: "))
+if km <= 200:
+    preco = km * 0.5
+else:
+    preco = km * 0.45
+print(f"A passagem custará {preco}")
