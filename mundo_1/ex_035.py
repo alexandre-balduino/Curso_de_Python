803478a8d91a1b4f7ab845b78e569ece2dcfbab8
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-04 18:26:33 -0300

    Resolvi o exercício 035

--- mundo_1/ex_035.py
+++ mundo_1/ex_035.py
@@ -6,3 +6,11 @@ comprimento de três retas e diga
 ao usuário se elas podem ou não 
 formar um triângulo.
 '''
+reta1 = int(input("Digite o tamanho da primeira reta: "))
+reta2 = int(input("Digite o tamanho da segunda reta: "))
+reta3 = int(input("Digite o tamanho da terceira reta: "))
+
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print("As retas acima podem formar um triângulo")
+else:
+    print("As retas acima não podem formar um triângulo")
