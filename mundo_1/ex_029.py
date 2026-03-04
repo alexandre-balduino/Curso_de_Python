cd5a5393acf05cea00a8a566884390dfb19c0753
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-04 17:29:36 -0300

    Resolvi o exercício 029

--- mundo_1/ex_029.py
+++ mundo_1/ex_029.py
@@ -9,3 +9,11 @@ multado.
 A multa vai custar R$7,00 por cada 
 Km acima do limite.
 '''
+
+km_h = int(input("Digite a velocidade do carro: "))
+if km_h > 80:
+    multa = 7 * (km_h - 80)
+    print(f"Excesso de velocidade! \nVocê foi multado em R${multa:.2f}")
+else:
+    print("Tenha uma boa viagem!")
+print("Dirija com cuidado")
