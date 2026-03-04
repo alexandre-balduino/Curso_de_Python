9bbbe63d044f0d0b8ed10d72a377dd032e0adabe
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-04 14:37:04 -0300

    Resolvi o exercício 028

--- mundo_1/ex_028.py
+++ mundo_1/ex_028.py
@@ -9,3 +9,13 @@ o número escolhido pelo computador.
 O programa deverá escrever na tela 
 se o usuário venceu ou perdeu.
 '''
+
+from random import randint
+
+print("Pensei em um número de 0 a 5.")
+n_comp = randint(0, 5)
+n_user = int(input("Tente adivinhar qual foi: "))
+if n_comp == n_user:
+    print("Parabéns! Você acertou.")
+else:
+    print(f"Você escolheu {n_user} mas o computador escolheu {n_comp}. Não foi dessa vez, tente de novo!")
