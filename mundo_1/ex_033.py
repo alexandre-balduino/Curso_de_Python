6973dea3715e4d7060704016e951169da115b924
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-04 18:09:12 -0300

    Resolvi o exercício 033

--- mundo_1/ex_033.py
+++ mundo_1/ex_033.py
@@ -5,3 +5,14 @@ Faça um programa que leia três
 números e mostre qual é o maior e 
 qual é o menor.
 '''
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+maior = num1
+if num2 > num1:
+    maior = num2
+if num3 > num1:
+    maior = num3
+print(f"O maior número é o {maior}")
