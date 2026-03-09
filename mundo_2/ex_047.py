d8bb4b37c6b00e7aa97d60bbd3aad6c2a80b7c0a
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-09 20:01:24 -0300

    Resolvi o exercício 047 do mundo 2

---
+++ mundo_2/ex_047.py
@@ -0,0 +1,10 @@
+'''
+Desafio 047
+
+Crie um programa que mostre na tela 
+todos os números pares que estão no 
+intervalo entre 1 e 50.
+'''
+
+for i in range(1, 51):
+    print(f"{i}, " if i%2==0 else "", end="")
