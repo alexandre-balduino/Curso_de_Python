e72ffd68cddab29cd6a765a09edb06037aa7bac0
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-10 18:55:44 -0300

    Resolvi o exercício 050 do mundo 2

---
+++ mundo_2/ex_050.py
@@ -0,0 +1,17 @@
+'''
+Deaafio 050
+
+Desenvolva um programa que leia 
+seis números inteiros e mostre a 
+soma apenas daqueles que forem pares. 
+Se o valor digitado for ímpar, 
+desconsidere-o.
+'''
+
+soma = 0
+for n in range(6):
+    num = int(input("Digite um número: "))
+    if num%2==0:
+        soma += num
+
+print(f"A soma dos números pares é {soma}")
