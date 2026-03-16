832d847060f3e710c1c519794764155e6974740f
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-15 22:25:54 -0300

    Resolvi o exercício 076 do mundo 3

--- mundo_3/ex_076.py
+++ mundo_3/ex_076.py
@@ -9,22 +9,24 @@ preços, organizando os dados em
 forma tabular.
 '''
 
-listagem = ('Lápis', 1.75,
-            'Borracha', 2.00,
-            'Caderno', 15.90,
-            'Estojo', 25.00,
-            'Transferidor', 4.20,
-            'Compasso', 9.99,
-            'Mochila', 120.32,
-            'Canetas', 22.30,
-            'Livro', 34.90)
+listagem = (
+    "Lápis", 1.75,
+    "Borracha", 2.00,
+    "Caderno", 15.90,
+    "Estojo", 25.00,
+    "Transferidor", 4.20,
+    "Compasso", 9.99,
+    "Mochila", 120.32,
+    "Canetas", 22.30,
+    "Livro", 34.90
+)
 
-print('-' * 32)
-print(f'{"LISTAGEM DE PREÇOS":^32}')
-print('-' * 32)
+print("-" * 32)
+print(f"{'LISTAGEM DE PREÇOS':^32}")
+print("-" * 32)
 for pos in range(0, len(listagem)):
     if pos % 2 == 0:
-        print(f'{listagem[pos]:.<23}', end='')
+        print(f"{listagem[pos]:.<23}", end="")
     else:
-        print(f'R${listagem[pos]:>7.2f}')
-print('-' * 32)
+        print(f"R${listagem[pos]:>7.2f}")
+print("-" * 32)
