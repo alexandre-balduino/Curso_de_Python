8b61174b66555252d182f46da1c4cc3e51df87a1
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-03-26 17:02:36 -0300

    resolvi o exercício 097 do mundo 3

--- mundo_3/ex_096.py
+++ mundo_3/ex_096.py
@@ -12,4 +12,5 @@ def area(larg, comp):
     resultado = larg * comp
     print(f"A área de um terreno {larg} x {comp} é {resultado}")
 
+
 area(50, 100)
---
+++ mundo_3/ex_097.py
@@ -0,0 +1,18 @@
+'''
+Desafio 097
+
+Faça um programa que tenha uma 
+função chamada escreva(), que 
+receba um texto qualquer como 
+parâmetro e mostre uma mensagem com 
+tamanho daptável.
+'''
+
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print("-" * tamanho)
+    print(f"  {texto}")
+    print("-" * tamanho)
+
+
+escreva("Olá, Mundo!")
