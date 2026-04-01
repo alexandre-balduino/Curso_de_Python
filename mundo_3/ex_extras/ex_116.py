bd5fc87190ecb7ea68a9f530c4ce99497a184d5b
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-04-01 10:36:23 -0300

    resolvi o exercício 116

--- mundo_3/ex_extras/ex_116.py
+++ mundo_3/ex_extras/ex_116.py
@@ -9,8 +9,11 @@ de texto e diga:
 
 import os
 
-print(os.getcwd())
-
-#arq = open("ex_113.txt", "r")
-
-#print(arq)
+arquivo = os.path.join(os.path.dirname(__file__), "ex_116.txt")
+with open(arquivo, "r", encoding="utf-8") as arq:
+    conteudo = arq.read()
+    arq.seek(0)
+    letras = sum(1 for l in conteudo if l.isalpha())
+    linhas = len(conteudo.splitlines())
+    print(conteudo)
+    print(f"O arquivo tem {linhas} linhas, e {letras} letras")
