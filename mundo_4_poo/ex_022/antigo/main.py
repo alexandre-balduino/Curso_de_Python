aa0cf499ce30d560d922a48ac49fee324c5890bd
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-04-14 10:11:03 -0300

    melhorei o exercício 022 do mundo 4

---
+++ mundo_4_poo/ex_022/antigo/main.py
@@ -0,0 +1,37 @@
+
+from os import system
+from rich import print
+from rich.panel import Panel
+from ex_022 import ControleRemoto
+
+def main():
+    controle = ControleRemoto()
+    while True:
+        system("clear")
+        if not controle.ligado:
+            visor = f"[red]A TV está desligada[/]"
+        else:
+            canais = [str(i) for i in range(1, 6)]
+            canais[controle.canal - 1] = f"[blue]{controle.canal}[/]"
+            canal = "CANAL = " + " ".join(canais)
+            barra_vol = "■" * controle.volume
+            espaco_vol = " " * (5 - controle.volume)
+            volume = f"VOLUME = [[blue]{barra_vol}[/]{espaco_vol}]"
+            visor = f"{canal}\n{volume}"
+            tv = Panel(visor, title="[ TV ]")
+            print(tv)
+        opcao = input(f"[ @ ] ON/OFF \n<CH{controle.canal}> \n-VOL{controle.volume}+\n").strip()
+        match opcao:
+            case "@":
+                controle.liga_desliga()
+            case "<":
+                controle.voltar_canal()
+            case ">":
+                controle.avancar_canal()
+            case "-":
+                controle.diminuir_volume()
+            case "+":
+                controle.aumentar_volume()
+
+
+main()
