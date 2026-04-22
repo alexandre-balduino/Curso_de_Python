916b1a0f84aaa22d2eaa4b2d43bea2bfe350f24c
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-04-22 16:26:16 -0300

    resolvi o exercício 023 do mundo 4 do curso de python

---
+++ mundo_4_poo/desafios/ex_023/__main__.py
@@ -0,0 +1,8 @@
+
+from ex_023 import *
+
+quad = Quadrado(5)
+print(f"O perímetro do quadrado é: {quad.perimetro()} \nA àrea é: {quad.area()}")
+
+circ = Circulo(5)
+print(f"O perímetro do círculo é: {circ.perimetro():.2f} \nA àrea é: {circ.area():.2f}")
--- mundo_4_poo/desafios/ex_023/ex_023.py
+++ mundo_4_poo/desafios/ex_023/ex_023.py
@@ -10,8 +10,8 @@ Implemente o seguinte diagrama de classe:
     | + perimetro() {abstract} |
     | + area() {abstract}      |
     |--------------------------|
-                   ∆
-                   |
+                  ∆
+                  |
         |-------------------|
         |                   |
 |---------------|    |---------------|
@@ -23,3 +23,48 @@ Implemente o seguinte diagrama de classe:
 | + area()      |    | + area()      |
 |---------------|    |---------------|
 '''
+
+import math
+from abc import ABC, abstractmethod
+
+class Poligono(ABC):
+    def __init__(self, qtd_lados):
+        self.qtd_lados = qtd_lados
+    
+    @abstractmethod
+    def perimetro(self):
+        """
+            Este método deve ser implementado pelas subclasses
+        """
+        pass
+    
+    @abstractmethod
+    def area(self):
+        """
+            Este método deve ser implementado pelas subclasses
+        """
+        pass
+
+
+class Quadrado(Poligono):
+    def __init__(self, lado, qtd_lados=4):
+        super().__init__(qtd_lados)
+        self.lado = lado
+    
+    def perimetro(self):
+        return self.lado * 4
+    
+    def area(self):
+        return self.lado ** 2
+
+
+class Circulo(Poligono):
+    def __init__(self, raio, qtd_lados=0):
+        super().__init__(qtd_lados)
+        self.raio = raio
+    
+    def perimetro(self):
+        return 2 * math.pi * self.raio
+    
+    def area(self):
+        return math.pi * (self.raio ** 2)
