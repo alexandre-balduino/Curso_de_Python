0f4fa0f0ae22f92fc63c4f9312f6fdd40ffb3eb8
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-05-19 19:44:36 -0300

    continuei o exercício 026 do mundo 4 do curso de python

--- mundo_4_poo/desafios/ex_026/__main__.py
+++ mundo_4_poo/desafios/ex_026/__main__.py
@@ -2,4 +2,5 @@
 from ex_026 import *
 
 func1 = Mensalista(nome="João", sal_bruto=3500)
-func2 = Horista(nome="George", horas_trab=200, valor_hora=20)
+func1.calcular_sal()
+func1.analisar_sal()
--- mundo_4_poo/desafios/ex_026/ex_026.py
+++ mundo_4_poo/desafios/ex_026/ex_026.py
@@ -33,10 +33,10 @@ salários de funciinários diferentes
 from abc import ABC, abstractmethod
 
 class Funcionario(ABC):
-    def __init__(self, nome, sal_bruto, sal_liquido, sal_minimo=1612, inss=7.5):
+    def __init__(self, nome, sal_minimo=1612, inss=7.5):
         self.nome = nome
-        self.sal_bruto = sal_bruto
-        self.sal_liquido = sal_liquido
+        self.sal_bruto = 0
+        self.sal_liquido = 0
         self.sal_minimo = sal_minimo
         self.inss = inss
     
@@ -45,12 +45,12 @@ class Funcionario(ABC):
         pass
     
     def analisar_sal(self):
-        pass
+        print(f"{self.nome} recebe {self.sal_liquido}")
 
 
 class Horista(Funcionario):
-    def __init__(self, nome, sal_bruto, sal_liquido, valor_hora, horas_trab):
-        super().__init__(nome, sal_bruto, sal_liquido)
+    def __init__(self, nome, valor_hora, horas_trab):
+        super().__init__(nome)
         self.valor_hora = valor_hora
         self.horas_trab = horas_trab
     
@@ -59,8 +59,9 @@ class Horista(Funcionario):
 
 
 class Mensalista(Funcionario):
-    def __init__(nome, sal_bruto, sal_liquido):
-        super().__init__(nome, sal_bruto, sal_liquido)
+    def __init__(self, nome, sal_bruto):
+        super().__init__(nome, sal_bruto)
     
     def calcular_sal(self):
-        pass
+        desc = self.sal_bruto * (self.inss / 100)
+        self.sal_liquido = self.sal_bruto - desc
